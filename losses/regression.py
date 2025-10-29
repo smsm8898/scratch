e455110af7a4b2910daedbac7287016540594edc
smsm8898 <smsm8898@gripcorp.co>
2025-10-29 20:02:13 +0900

    expr: regression loss function

--- losses/regression.py
+++ losses/regression.py
@@ -17,3 +17,24 @@ class MAE:
     def backward(self, y_true, y_pred):
         m = len(y_true)
         return -np.sign(y_true, y_pred) / m
+    
+class Huber:
+    name = "huber"
+    def __init__(self, delta=0.1):
+        self.delta = delta
+
+    def forward(self, y_true, y_pred):
+        diff = y_true - y_pred
+        condition = np.abs(diff) <= self.delta
+        mse = 0.5 * diff **2 # small
+        mae = self.delta * np.abs(diff) - 0.5 * self.delta # large
+        loss = np.where(condition, mse, mae)
+        return np.mean(loss)
+    
+    def backward(self, y_true, y_pred):
+        diff = y_true - y_pred
+        condition = np.abs(diff) <= self.delta
+        mse = - diff # small
+        mae = - self.delta * np.sign(diff) # large
+        grad = np.where(condition, mse, mae)
+        return grad / len(y_true)
